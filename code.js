f0cd8ef5c69766f037e155f20188d4668602c124
MatthewJesseThomas <owlsaintgermain@gmail.com>
2023-02-06 09:46:47 +0200

    main

---
+++ code.js
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(__dirname);
+// console.log(__filename);
+// const counter = function(arr){
+//     return 'There are ' + arr.length + ' elements in this Array';
+// };
+// console.log(counter(['Draco', 'Piaty', 'Zeldris', 'Momonga']));
